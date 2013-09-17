ae266c42b67d1ea13cddf4b25d869f1b2be439f7
rhubarbselleven <drew@olvec.net>
2013-09-17 17:20:38 +1000

    Equipment layout on the way

--- app/scripts/views/unit/UnitEquipment.js
+++ app/scripts/views/unit/UnitEquipment.js
@@ -8,7 +8,7 @@
 define([
     'marionette',
 
-    'tpl!templates/unit/default.html'
+    'tpl!templates/unit/unitEquipment.html'
 
 ], function (Marionette, template) {
     return Marionette.ItemView.extend({
