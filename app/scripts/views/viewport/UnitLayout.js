0ca64852c257f6211a9172d348fd238b964434d9
rhubarbselleven <drew@olvec.net>
2013-09-19 15:17:22 +1000

    armor layout implemented and layout reshuffle

--- app/scripts/views/viewport/UnitLayout.js
+++ app/scripts/views/viewport/UnitLayout.js
@@ -10,12 +10,13 @@ define([
     'tpl!templates/viewport/unitLayout.html',
     'views/unit/UnitHeader',
     'views/unit/UnitOverview',
+    'views/unit/UnitArmor',
     'views/unit/UnitEquipment',
     'views/unit/UnitAmmo',
     'views/unit/UnitWeaponry',
     'views/unit/UnitRanges',
     'views/unit/UnitSlotLayout'
-], function (Marionette, template, UnitHeader, UnitOverview, UnitEquipment, UnitAmmo, UnitWeaponry, UnitRanges, UnitSlotLayout) {
+], function (Marionette, template, UnitHeader, UnitOverview, UnitArmor, UnitEquipment, UnitAmmo, UnitWeaponry, UnitRanges, UnitSlotLayout) {
     "use strict";
 
     return Marionette.Layout.extend({
@@ -24,6 +25,7 @@ define([
         regions: {
             header: ".header",
             overview: ".overview",
+            armor: ".armor",
             equipment: ".equipment",
             ammo: ".ammo",
             weaponry: ".weaponry",
@@ -39,6 +41,7 @@ define([
             var model = {model: this.model};
             this.header.show(new UnitHeader(model));
             this.overview.show(new UnitOverview(model));
+            this.armor.show(new UnitArmor(model));
             this.equipment.show(new UnitEquipment(model));
             this.ammo.show(new UnitAmmo(model));
             this.weaponry.show(new UnitWeaponry(model));
